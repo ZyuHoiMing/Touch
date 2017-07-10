917549f2e449c7f3fb785fa2df46867a6d818eb9
Tony Zhu <tony.zzx.cn@gmail.com>
2017-07-10 09:15:36 +0800

    更新hamburger

---
+++ Touch/Models/HambugerMenuListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Touch.Models
+{
+    public class HambugerMenuListItem
+    {
+        public Symbol ItemSymbol { get; set; }
+        public string ItemName { get; set; }
+    }
+}
--- Touch/Views/MainPage.xaml.cs
+++ Touch/Views/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Touch.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,44 @@ namespace Touch.Views
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<HambugerMenuListItem> hambugerMenuPrimaryItems;
+        private ObservableCollection<HambugerMenuListItem> hambugerMenuSecondaryItems;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            hambugerMenuPrimaryItems = new ObservableCollection<HambugerMenuListItem>();
+            hambugerMenuSecondaryItems = new ObservableCollection<HambugerMenuListItem>();
+        }
+        
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            HambugerMenuListItem item = new HambugerMenuListItem()
+            {
+                ItemName = "首页",
+                ItemSymbol = Symbol.Home
+            };
+            hambugerMenuPrimaryItems.Add(item);
+
+            HambugerMenuListItem item2 = new HambugerMenuListItem()
+            {
+                ItemName = "设置",
+                ItemSymbol = Symbol.Setting
+            };
+            hambugerMenuSecondaryItems.Add(item2);
+        }
+
+        private void HambugerMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainPageSplitView.IsPaneOpen = !MainPageSplitView.IsPaneOpen;
+        }
+
+        private void HambugerMenuPrimaryList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //ListView a = (ListView)sender;
+            //a.SelectedItem;
+            MainPageFrame.Navigate(typeof(_3DGalleryPage));
         }
     }
 }
