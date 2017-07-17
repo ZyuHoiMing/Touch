b2044c0c9a5065053e3c1097815bbc9318a3f8d9
梁 <807781029@qq.com>
2017-07-17 16:51:07 +0800

    选择图片文件夹

--- Touch/Views/Pages/SettingPage.xaml.cs
+++ Touch/Views/Pages/SettingPage.xaml.cs
@@ -36,131 +36,24 @@ namespace Touch.Views.Pages
         }
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            //打开文件选择器
-            String folderStr = "photo";
-            FolderPicker pick = new FolderPicker();
-            pick.FileTypeFilter.Add(".png");
-            pick.FileTypeFilter.Add(".jpg");
-            pick.FileTypeFilter.Add(".bmp");
-            IAsyncOperation<StorageFolder> folderTask = pick.PickSingleFolderAsync();
-
-            StorageFolder folder = await folderTask;
-
-            //var folder = await pick.PickSingleFolderAsync();
-            StorageFolder Folder = null;
-            string Address;
-            string Token = "";
-            if (folder != null)
+            FileOpenPicker filePicker = new FileOpenPicker();
+            filePicker.ViewMode = PickerViewMode.Thumbnail;
+            filePicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            filePicker.FileTypeFilter.Add(".png");
+            filePicker.FileTypeFilter.Add(".jpg");
+            //Windows.Storage.StorageFile file = await filePicker.PickSingleFileAsync();
+            var files = await filePicker.PickMultipleFilesAsync();
+            foreach (Windows.Storage.StorageFile file in files)
             {
-                Folder = folder;
-                Address = folder.Path;
-                Token = StorageApplicationPermissions.FutureAccessList.Add(folder);
-            }
-            //StorageApplicationPermissions.FutureAccessList.GetFolderAsync(Token);
-            Debug.WriteLine(Token);
-            //获取本地文件夹
-            StorageFolder folderLocal = ApplicationData.Current.LocalFolder;
-
-            //创建一个文件夹account
-            try
-            {
-                folderLocal = await folderLocal.GetFolderAsync(folderStr);
-            }
-            catch (FileNotFoundException)
-            {
-                folderLocal = await folderLocal.CreateFolderAsync(folderStr);
-            }
-
-            StorageFile file = await folderLocal.CreateFileAsync(folderStr + ".json", CreationCollisionOption.ReplaceExisting);
-
-            //保存选择的文件夹Token
-            var json = JsonSerializer.Create();
-            Debug.WriteLine(file.Name + file.Path);
-            ImagePath imagePath = new ImagePath { Id = DateTime.Now.ToString("yyMMddHHmmss"), Path = Token };
-            var resault = JsonConvert.SerializeObject(imagePath);
-            //string imageJson = resault.Stringify();
-            if (file != null)
-            {
-                try
+                Windows.UI.Xaml.Media.Imaging.BitmapImage bitmap = new Windows.UI.Xaml.Media.Imaging.BitmapImage();
+                using (var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite))
                 {
-                    using (StorageStreamTransaction transaction = await file.OpenTransactedWriteAsync())
-                    {
-                        using (DataWriter dataWriter = new DataWriter(transaction.Stream))
-                        {
-                            dataWriter.WriteInt32(Encoding.UTF8.GetByteCount(resault));
-                            dataWriter.WriteString(resault);
-                            transaction.Stream.Size = await dataWriter.StoreAsync();
-                            await transaction.CommitAsync();
-                        }
-                    }
+                    bitmap.SetSource(stream);
                 }
-                catch (Exception ex)
-                {
-                    throw ex;
-                }
+                Image img = new Image();
+                img.Source = bitmap;
+                imgView.Items.Add(img);
             }
-        }
-
-        private async void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            /*string folderStr = "photo";
-            StorageFile fileLocal = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appdata:///local/" + folderStr + ".json"));
-            if (fileLocal != null)
-            {
-                try
-                {
-                    //读取本地文件内容，并且反序列化
-                    using (IRandomAccessStream readStream = await fileLocal.OpenAsync(FileAccessMode.Read))
-                    {
-                        using (DataReader dataReader = new DataReader(readStream))
-                        {
-                            UInt64 size = readStream.Size;
-                            if (size <= UInt32.MaxValue)
-                            {
-                                await dataReader.LoadAsync(sizeof(Int32));
-                                Int32 stringSize = dataReader.ReadInt32();
-                                await dataReader.LoadAsync((UInt32)stringSize);
-                                string fileContent = dataReader.ReadString((uint)stringSize);
-                                ImagePath imagePath = new ImagePath(fileContent);
-                                StorageFolder folder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync(imagePath.Path);
-                                //筛选图片
-                                var queryOptions = new Windows.Storage.Search.QueryOptions();
-                                queryOptions.FileTypeFilter.Add(".png");
-                                queryOptions.FileTypeFilter.Add(".jpg");
-                                queryOptions.FileTypeFilter.Add(".bmp");
-                                var query = folder.CreateFileQueryWithOptions(queryOptions);
-                                var files = await query.GetFilesAsync();
-
-                                ImagePath img;
-                                ObservableCollection<ImagePath> imgList = new ObservableCollection<ImagePath>();
-                                foreach (var item in files)
-                                {
-                                    IRandomAccessStream irandom = await item.OpenAsync(FileAccessMode.Read);
-
-                                    //对图像源使用流源
-                                    BitmapImage bitmapImage = new BitmapImage();
-                                    bitmapImage.DecodePixelWidth = 160;
-                                    bitmapImage.DecodePixelHeight = 100;
-                                    await bitmapImage.SetSourceAsync(irandom);
-
-                                    img = new ImagePath();
-                                    img.Path = item.Path;
-                                    img.File = bitmapImage;
-                                    img.Storage = item;
-                                    imgList.Add(img);
-                                }
-                                imageView.ItemsSource = imgList;
-                            }
-
-                        }
-                    }
-                }
-                catch (Exception exce)
-                {
-                    await new MessageDialog(exce.ToString()).ShowAsync();
-                    throw exce;
-                }
-            }*/
         }
     }
 }
