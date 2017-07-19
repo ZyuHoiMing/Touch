d80e942054cdc2cfb97b4075742ffb3ec3eca46e
ZYH_DayDayUp <952959129@qq.com>
2017-07-19 15:20:18 +0800

    实现自动移动

---
+++ Touch/Views/pathPov.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+namespace Touch.Views
+{
+    /// <summary>
+    /// 设置路径的视角
+    /// </summary>
+    public class pathPov
+    {
+        private Point from;
+        private Point to;
+        public pathPov(Point from,Point to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+         public int getHeading()
+        {
+            double tmpY = to.X - from.X;//纬度
+            double tmpX = to.Y - from.Y;//经度
+            if (System.Math.Abs(tmpY)>System.Math.Abs(tmpX))
+            {
+                if (tmpY > 0) return 0;
+                else return 180;
+            }
+            else
+            {
+                if (tmpX > 0) return 90;
+                else return 270;
+            }
+        }
+    }
+}
--- Touch/Views/webview.xaml.cs
+++ Touch/Views/webview.xaml.cs
@@ -14,6 +14,9 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Web.Http;
+using Windows.UI.Core;
+using Windows.System.Threading;
+using Windows.Foundation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
 
@@ -24,6 +27,7 @@ namespace Touch.Views
     /// </summary>
     public sealed partial class webview : Page
     {
+        private List<Point> pathPoint=new List<Point>();
         public webview()
         {
             this.InitializeComponent();
@@ -37,6 +41,122 @@ namespace Touch.Views
             else {
                 Uri uri = new Uri("ms-appx-web:///web/test.html");
                 webview1.Navigate(uri);
+            }
+            //insert
+            pathPoint.Add(new Point(22.277782, 114.170241));
+            pathPoint.Add(new Point(22.277753, 114.169953));
+            pathPoint.Add(new Point(22.277759, 114.169793));
+            pathPoint.Add(new Point(22.277759, 114.169415));
+            pathPoint.Add(new Point(22.277759, 114.169179));
+            pathPoint.Add(new Point(22.277841, 114.168825));
+            pathPoint.Add(new Point(22.277968, 114.168482));
+            pathPoint.Add(new Point(22.278107, 114.168546));
+        }
+        public async void invokeJsStart(string  x,string y)
+        {
+            string[] script = { "panorama=new google.maps.StreetViewPanorama("+
+            "document.getElementById('street-view'),"+
+            "{position: { lat: "+x+", lng:"+y+"},"+
+          "pov: { heading: 90, pitch: 0},"+
+            "});" };
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+             {
+                 string result = await webview1.InvokeScriptAsync("eval", script);
+                //Debug.WriteLine(result);
+            });
+        }
+        public async void invokeJsMove(string x, string y, string heading)
+        {
+            string[] script = { "panorama.setPosition({lat:"
+            +x+",lng:"+y
+            +"});panorama.setPov({"+
+            "heading:"+heading+",pitch:0})" };
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+            {
+                string result = await webview1.InvokeScriptAsync("eval", script);
+                //Debug.WriteLine(result);
+            });
+        }
+        public void show_path(int nodeNum)
+        {
+            bool completed = false;
+            TimeSpan delay = TimeSpan.FromSeconds(2);
+            ThreadPoolTimer DelayTimer = ThreadPoolTimer.CreateTimer
+            ((source) =>
+            {
+                //
+                // TODO: Work
+                //
+                string x=pathPoint.ElementAt(nodeNum).X.ToString();
+                string y = pathPoint.ElementAt(nodeNum).Y.ToString();
+                Debug.WriteLine(x);
+                Debug.WriteLine(y);
+                pathPov pathpov = new pathPov(pathPoint.ElementAt(nodeNum-1),pathPoint.ElementAt(nodeNum));
+                string tmpheading=pathpov.getHeading().ToString();
+                invokeJsMove(x,y,tmpheading);
+                //
+                // Update the UI thread by using the UI core dispatcher.
+                //
+                completed = true;
+            }, delay, async (source) =>
+            {
+                //
+                // TODO: Handle work cancellation/completion.
+                //
+
+
+                //
+                // Update the UI thread by using the UI core dispatcher.
+                //
+                await Dispatcher.RunAsync(
+                    CoreDispatcherPriority.High,
+                    () =>
+                    {
+                        //
+                        // UI components can be accessed within this scope.
+                        //
+
+                        if (completed)
+                        {
+                            if (nodeNum == pathPoint.Count-1)
+                            {
+                                Debug.WriteLine("finish");
+                            }
+                            else
+                            {
+                                show_path(nodeNum + 1);
+                            }
+                            // Timer completed.
+                        }
+                        else
+                        {
+                            // Timer cancelled.
+                        }
+                    });
+            });
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (pathPoint.Count == 0)
+            {
+                Debug.WriteLine("no path");
+            }
+            else
+            {
+                string x = pathPoint.ElementAt(0).X.ToString();
+                string y = pathPoint.ElementAt(0).Y.ToString();
+                invokeJsStart(x, y);
+                TimeSpan delay = TimeSpan.FromSeconds(2);
+                ThreadPoolTimer DelayTimer = ThreadPoolTimer.CreateTimer
+                ((source) =>
+                {
+                    if (pathPoint.Count > 1)
+                        show_path(1);
+                    else
+                    {
+                        Debug.WriteLine("can't move");
+                    }
+                }, delay);
             }
         }
     }
